ea3667de239a791d90bf4e523417b523d4555ae5
GeumjuCho <751352@gmail.com>
2018-08-22 18:23:18 +0900

    이미지,동영상 추가하기

---
+++ image추가하기.py
@@ -0,0 +1,7 @@
+# 이미지 추가하기
+from IPython.display import Image
+Image(filename='data10/7_group_by/group_by_cities.png') # 해당이미지가 있는 주소를 넣기
+
+# 동영상 추가하기-유튜브와 연동
+from IPython.display import YouTubeVideo
+YouTubeVideo("KKGfjhs_26M") #유튜브의 주소 맨뒤 명만 넣기
